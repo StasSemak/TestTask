b0873188de9513738efe4290622f49eaa031e540
StasSemak <semaks051@gmail.com>
2023-08-25 20:28:24 +0300

    add: requests list

---
+++ src/components/requests/RequestCard.tsx
@@ -0,0 +1,43 @@
+import { Calendar, ClipboardList, PackageOpen, Pencil, Trash2 } from "lucide-react"
+import Button from "../ui/Button"
+
+const RequestCard = (request: ParcelRequest) => {
+    const isOrder = request.requestType === 'order'
+
+    const isDate = new Date(request.dateDispatch).getTime() !== new Date(0).getTime()
+
+    return(
+        <div className="request-card">
+            <p className="text-xl font-bold type">{request.requestType}</p>
+            <p className="text-2xl font-medium">{request.from} - {request.to}</p>
+            <div className="flex gap-1.5 items-center">
+                <Calendar className="h-6 w-6 mb-1 icon"/>
+                <p className="text-lg font-medium">
+                    {isDate ? new Date(request.dateDispatch).toLocaleDateString() : "Not specified"}
+                </p>
+            </div>
+            {isOrder && 
+                <>
+                    <div className="flex gap-1.5 items-center">
+                        <PackageOpen className="icon"/>
+                        <p className="text-lg font-medium">{request.parcelType}</p>
+                    </div>
+                    <div className="flex gap-1.5 items-center">
+                        <ClipboardList className="icon"/>
+                        <p className="font-medium">{request.description}</p>
+                    </div>
+                </>
+            }
+            <div className="flex justify-end gap-3 mt-2">
+                <Button>
+                    <Pencil className="h-4 w-4"/>
+                </Button>
+                <Button>
+                    <Trash2 className="h-4 w-4"/>
+                </Button>
+            </div>
+        </div>
+    )
+}
+
+export default RequestCard
--- src/components/requests/Requests.tsx
+++ src/components/requests/Requests.tsx
@@ -1,6 +1,26 @@
+import { useEffect, useState } from "react"
+import { getRequests } from "../../lib/requests"
+import RequestCard from "./RequestCard"
+
 const Requests = () => {
+    const [requests, setRequests] = useState<ParcelRequest[]>([])
+
+    useEffect(() => {
+        const reqs = getRequests()
+        if(reqs) setRequests(reqs)
+    }, [])
+
     return(
-        <div>requests page</div>
+        <div className="requests">
+            <h2>Your requests</h2>
+            <div className="requests-container">
+                {requests.map((item, index) => {
+                    const { requestType, ...props } = item
+
+                    return <RequestCard key={index} requestType={requestType} {...props}/>
+                })}
+            </div>
+        </div>
     )
 }
 
